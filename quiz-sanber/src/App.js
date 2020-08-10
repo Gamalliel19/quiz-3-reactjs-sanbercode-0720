f503899749e2b808f236a523fd590bc616e8a999
Gamalliel Sharon <you@example.com>
2020-08-10 12:34:22 +0700

    menambahkan quiz pekan ke 3 react js

--- quiz-sanber/src/App.js
+++ quiz-sanber/src/App.js
@@ -1,16 +1,39 @@
-import React, { Component } from 'react';
-import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom'
-import Navbar from './parts/Navbar';
+// import React, { Component } from 'react';
+// import './App.css';
+// import { BrowserRouter as Router } from 'react-router-dom'
+// import Navbar from './parts/Navbar';
 
-class App extends Component {
-  render() {
-    return (
+// class App extends Component {
+//   render() {
+//     return (
+//       <Router>
+//         <Navbar />
+//       </Router>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React from 'react';
+import {BrowserRouter as Router} from "react-router-dom";
+import './App.css';
+import './quiz/style.css';
+import Routes from './quiz/Routes';
+import {LoginProvider} from './quiz/LoginContext';
+
+function App() {
+  return (
+    <>
+    <div>
+      <LoginProvider>
       <Router>
-        <Navbar />
+        <Routes/>
       </Router>
-    );
-  }
+      </LoginProvider>
+    </div>
+    </>
+  );
 }
 
 export default App;
